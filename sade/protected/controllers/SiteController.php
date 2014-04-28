bf3abe015be51bc01ef6161d558d54eb59cf024b
SADE-2014 <digonzal@alumnos.ubiobio.cl>
2014-04-28 13:00:52 -0400

    Revert "cambio main - gui"

--- sade/protected/controllers/SiteController.php
+++ sade/protected/controllers/SiteController.php
@@ -7,7 +7,7 @@ class SiteController extends Controller
 	 */
 	public function actions()
 	{
-		$this->layout ='/layouts/main';
+		$this->layout ='/layouts/gui';
 		return array(
 			// captcha action renders the CAPTCHA image displayed on the contact page
 			'captcha'=>array(
@@ -28,7 +28,7 @@ class SiteController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$this->layout ='/layouts/main';
+		$this->layout ='/layouts/gui';
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
 		
--- sade/protected/views/espacioComun/admin.php
+++ sade/protected/views/espacioComun/admin.php
@@ -12,7 +12,7 @@ $this->menu=array(
 	array('label'=>'Crear Espacio Común', 'url'=>array('create')),
 );
 
-Yii::app()->clientScript->registerScript('search', "
+Yii::app()->clientScript->registerScript('search', "s
 $('.search-button').click(function(){
 	$('.search-form').toggle();
 	return false;

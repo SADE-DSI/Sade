666447d9e540803f50a244d7bf2682b9e0451eba
leo14 <leo14fernandez@gmail.com>
2014-04-28 14:37:16 -0400

    Gestionar Residentes
    Mostrar todos campos de dos tablas

--- sade/protected/views/arrendatarioDueno/_view.php
+++ sade/protected/views/arrendatarioDueno/_view.php
@@ -21,5 +21,28 @@
 	<?php echo CHtml::encode($data->adFechaLiberacion); ?>
 	<br />
 
+	<b><?php echo CHtml::encode($data->getAttributeLabel('peNombresApellidos')); ?>:</b>
+	<?php echo CHtml::encode($data->ad_pe->peNombresApellidos); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('peActivo')); ?>:</b>
+	<?php echo CHtml::encode($data->ad_pe->peActivo); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('peEmail')); ?>:</b>
+	<?php echo CHtml::encode($data->ad_pe->peEmail); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('peTelefono')); ?>:</b>
+	<?php echo CHtml::encode($data->ad_pe->peTelefono); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('peDescripcion')); ?>:</b>
+	<?php echo CHtml::encode($data->ad_pe->peDescripcion); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('peDireccion')); ?>:</b>
+	<?php echo CHtml::encode($data->ad_pe->peDireccion); ?>
+	<br />
 
 </div>

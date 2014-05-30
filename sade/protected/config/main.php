7b7f5682e636fb4af311ac3519fe85b53f009002
Sam1991 <samuel_03061991@hotmail.com>
2014-05-30 16:27:24 -0400

    Cambio interfaz

--- sade/protected/config/main.php
+++ sade/protected/config/main.php
@@ -9,7 +9,7 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'SADE',
 	'language'=>'es',
-	
+	'theme'=>"classic",
 
 	// preloading 'log' component
 	'preload'=>array('log'),
